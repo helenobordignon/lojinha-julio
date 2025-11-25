5a7817754fae960f4fdea63a68682da5c3803d22
Heleno Bordignon <135024897+helenobordignon@users.noreply.github.com>
2025-11-24 22:02:34 -0300

    variáveis

--- src/main/java/br/com/lojinha/LojinhaApp.java
+++ src/main/java/br/com/lojinha/LojinhaApp.java
@@ -3,7 +3,15 @@ package br.com.lojinha;
 public class LojinhaApp {
 
     public static void main(String[] args) {
+        System.out.println("Bem-vindo à Lojinha!");
 
+        /* Declaração de variáveis */
+        String produto = "Camisa";
+        int quantidade = 3;
+        double preco = 49.90;
+        boolean emEstoque = true;
+
+        System.out.println("Produto: " + produto + " | Preço: R$ " + preco);
     }
 
 }
